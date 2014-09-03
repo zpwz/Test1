0a84a6b03bdcffccba84b160ce65c162d76fcb8a
zpwz <1226764058@qq.com>
2014-09-03 15:11:40 +0800

    文件

--- src/com/cn/test/Test.java
+++ src/com/cn/test/Test.java
@@ -11,10 +11,7 @@ public class Test {
 		System.out.println(3);
 		System.out.println(4);
 		System.out.println(5);
-		System.out.println(7);
-		System.out.println(8);
-		System.out.println(9);
-		System.out.println(10);
+	
 
 		
 	}
