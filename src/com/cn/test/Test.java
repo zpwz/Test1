c9988bca48479cdc2bd6b54a4f4430c71f4dea6c
zpwz <1226764058@qq.com>
2014-09-03 15:56:28 +0800

    测试1

--- src/com/cn/test/Test.java
+++ src/com/cn/test/Test.java
@@ -8,12 +8,5 @@ public class Test {
 		System.out.println("ÄãºÃ£¬java£¬ÄãºÃÂğ£¿");
 		System.out.println(1);
 		System.out.println(2);
-		System.out.println(3);
-		System.out.println(4);
-		System.out.println(5);
-	
-
-		
 	}
-
 }
