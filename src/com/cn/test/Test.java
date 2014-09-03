0bb418612605d24d414a362c4950e090f3e27ab6
zpwz <1226764058@qq.com>
2014-09-03 14:30:26 +0800

    master_1提交

--- src/com/cn/test/Test.java
+++ src/com/cn/test/Test.java
@@ -4,26 +4,19 @@ public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
 		System.out.println("ÄãºÃ£¬java£¬ÄãºÃÂğ£¿");
-
-		System.out.println("2");
-		System.out.println("3");
-
-		System.out.println("1");
+		System.out.println(1);
+		System.out.println(2);
+		System.out.println(3);
 		System.out.println(4);
 		System.out.println(5);
-
-
-		System.out.println("ÄãºÃ£¬java");
 		System.out.println(7);
-
 		System.out.println(8);
 		System.out.println(9);
 		System.out.println(10);
 
-
-
+		
 	}
 
 }
